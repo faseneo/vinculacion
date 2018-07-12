9ff970edc13d9c623799f3aa86be26f79218ed1e
fsegovia <francisco.segovia@umce.cl>
2018-07-12 17:06:16 -0400

    modifico header

--- header.php
+++ header.php
@@ -25,16 +25,4 @@
         <!-- Placed at the end of the document so the pages load faster -->
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
         <script src="js/jquery/1.12.4/jquery.min.js"></script>
-        <!-- <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script> -->
-        <!-- <script src="js/bootstrap.min.js"></script> -->
-
-                    <!-- <script src="bootstrap-datepicker/js/moment-with-locales.js"></script> -->
-
         <script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
-
-                    <!-- <script src="bootstrap-datepicker/js/bootstrap-datetimepicker.js"></script> -->
-
-        <!-- <script src="bootstrap-datepicker-1.6.4-dist/js/bootstrap-datepicker.min.js"></script>
-        <script src="bootstrap-datepicker-1.6.4-dist/locales/bootstrap-datepicker.en-GB.min.js" charset="UTF-8"></script>
-        <script src="bootstrap-datepicker-1.6.4-dist/locales/bootstrap-datepicker.es.min.js" charset="UTF-8"></script>
-         -->
